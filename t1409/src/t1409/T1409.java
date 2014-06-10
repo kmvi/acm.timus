3548d9d4d0ad531b382f3a88d22b269606c74213
kmvi <kmvi@users.noreply.github.com>
2014-06-10 12:44:46 +0400

    problem 1409

---
+++ t1409/src/t1409/T1409.java
@@ -0,0 +1,25 @@
+// http://acm.timus.ru/problem.aspx?space=1&num=1409
+
+package t1409;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class T1409 {
+
+	public static void main(String[] args) {
+		try {
+			Scanner inFile = new Scanner(System.in);
+			PrintStream outFile = new PrintStream(System.out);
+			
+			int a = inFile.nextInt();
+			int b = inFile.nextInt();
+			
+			outFile.println(String.format("%d %d", b-1, a-1));
+			
+			inFile.close();
+			outFile.close();
+		} catch (Exception ex) {}
+	}
+
+}
