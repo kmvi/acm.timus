0c1ba191a2c3af667c42f24659756eeccc820ee0
kmvi <kmvi@users.noreply.github.com>
2014-06-10 12:40:29 +0400

    problem 1293

---
+++ t1293/src/t1293/T1293.java
@@ -0,0 +1,26 @@
+// http://acm.timus.ru/problem.aspx?space=1&num=1293
+
+package t1293;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class T1293 {
+
+	public static void main(String[] args) {
+		try {
+			Scanner inFile = new Scanner(System.in);
+			PrintStream outFile = new PrintStream(System.out);
+			
+			int N = inFile.nextInt();
+			int A = inFile.nextInt();
+			int B = inFile.nextInt();
+			
+			outFile.println(2*N*A*B);
+			
+			inFile.close();
+			outFile.close();
+		} catch (Exception ex) {}
+	}
+
+}
