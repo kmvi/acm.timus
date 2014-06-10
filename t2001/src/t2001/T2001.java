fc4935f932f230c4379404b5c960f3b161c914ba
kmvi <kmvi@users.noreply.github.com>
2014-06-10 13:02:26 +0400

    problem 2001

---
+++ t2001/src/t2001/T2001.java
@@ -0,0 +1,29 @@
+// http://acm.timus.ru/problem.aspx?space=1&num=2001
+
+package t2001;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class T2001 {
+
+	public static void main(String[] args) {
+		try {
+			Scanner inFile = new Scanner(System.in);
+			PrintStream outFile = new PrintStream(System.out);
+			
+			int a1 = inFile.nextInt();
+			int b1 = inFile.nextInt();
+			int a2 = inFile.nextInt();
+			int b2 = inFile.nextInt();
+			int a3 = inFile.nextInt();
+			int b3 = inFile.nextInt();
+			
+			outFile.println(String.format("%d %d", a1-a3, b1-b2));
+			
+			inFile.close();
+			outFile.close();
+		} catch (Exception ex) {}
+	}
+
+}
