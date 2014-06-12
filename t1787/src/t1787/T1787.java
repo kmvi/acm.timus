49a272016255870c0bccd5ed20d7722caca1102d
kmvi <kmvi@users.noreply.github.com>
2014-06-12 07:26:02 +0400

    problem 1787

---
+++ t1787/src/t1787/T1787.java
@@ -0,0 +1,34 @@
+// http://acm.timus.ru/problem.aspx?space=1&num=1787
+
+package t1787;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class T1787 {
+
+	public static void main(String[] args) {
+		try {
+			Scanner inFile = new Scanner(System.in);
+			PrintStream outFile = new PrintStream(System.out);
+			
+			int k = inFile.nextInt();
+			int n = inFile.nextInt();
+			
+			int queue = 0;
+			for (int i = 0; i < n; ++i) {
+				int a = inFile.nextInt();
+				queue += a;
+				queue -= k;
+				if (queue < 0)
+					queue = 0;
+			}
+			
+			outFile.println(queue);
+			
+			inFile.close();
+			outFile.close();
+		} catch (Exception ex) {}
+	}
+
+}
