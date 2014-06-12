7f9f5da7d68e0f233a595f939f69380f3c97c5c0
kmvi <kmvi@users.noreply.github.com>
2014-06-12 17:30:45 +0400

    problem 1924

---
+++ t1924/src/t1924/T1924.java
@@ -0,0 +1,32 @@
+// http://acm.timus.ru/problem.aspx?space=1&num=1924
+
+// не важно, какой знак ставить, поэтому все сводитс€ к определению
+// четности или нечетности суммы 1..n
+
+package t1924;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class T1924 {
+
+	public static void main(String[] args) {
+		try {
+			Scanner inFile = new Scanner(System.in);
+			PrintStream outFile = new PrintStream(System.out);
+			
+			int n = inFile.nextInt();
+			int k = n*(n+1)/2;
+			
+			if (k % 2 == 0) {
+				outFile.println("black");
+			} else {
+				outFile.println("grimy");
+			}
+			
+			inFile.close();
+			outFile.close();
+		} catch (Exception ex) {}
+	}
+
+}
