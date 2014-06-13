30c08599378ea0fc43247c73a21b661c67daf84b
kmvi <kmvi@users.noreply.github.com>
2014-06-13 15:43:20 +0400

    problem 1636

---
+++ t1636/src/t1636/T1636.java
@@ -0,0 +1,31 @@
+package t1636;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class T1636 {
+
+	public static void main(String[] args) {
+		try {
+			Scanner inFile = new Scanner(System.in);
+			PrintStream outFile = new PrintStream(System.out);
+			
+			int T1 = inFile.nextInt();
+			int T2 = inFile.nextInt();
+			
+			int sum = 0;
+			for (int i = 0; i < 10; ++i) {
+				sum += inFile.nextInt();
+			}
+			
+			if (T1 <= T2-20*sum)
+				outFile.println("No chance.");
+			else
+				outFile.println("Dirty debug :(");
+			
+			inFile.close();
+			outFile.close();
+		} catch (Exception ex) {}
+	}
+
+}
