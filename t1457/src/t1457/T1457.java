a09d8cdb5279232b15a59cb2138b207545b40b6d
kmvi <kmvi@users.noreply.github.com>
2014-06-15 14:40:58 +0400

    problem 1457

---
+++ t1457/src/t1457/T1457.java
@@ -0,0 +1,27 @@
+package t1457;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class T1457 {
+
+	public static void main(String[] args) {
+		try {
+			Scanner inFile = new Scanner(System.in);
+			PrintStream outFile = new PrintStream(System.out);
+			
+			int n = inFile.nextInt();
+			
+			long sum = 0;
+			for (int i = 0; i < n; ++i) {
+				sum += inFile.nextLong();
+			}
+			
+			outFile.println(String.format("%.6f", (double)sum / n));
+			
+			inFile.close();
+			outFile.close();
+		} catch (Exception ex) {}
+	}
+
+}
