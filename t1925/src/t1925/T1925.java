dccea62728ee568b4bda585d91f3406974d6930f
kmvi <kmvi@users.noreply.github.com>
2014-08-07 16:17:03 +0400

    problem 1925

---
+++ t1925/src/t1925/T1925.java
@@ -0,0 +1,33 @@
+package t1925;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class T1925 {
+
+	public static void main(String[] args) {
+		try {
+			Scanner inFile = new Scanner(System.in);
+			PrintStream outFile = new PrintStream(System.out);
+			
+			int n = inFile.nextInt();
+			int k = inFile.nextInt();
+			
+			int sumExpected = k - 2;
+			int sumFact = 0;
+			for (int i = 0; i < n; ++i) {
+				sumExpected += (inFile.nextInt() - 2);
+				sumFact += inFile.nextInt();
+			}
+			
+			if (sumExpected >= sumFact)
+				outFile.println(sumExpected - sumFact);
+			else
+				outFile.println("Big Bang!");
+			
+			inFile.close();
+			outFile.close();
+		} catch (Exception ex) {}
+	}
+
+}
