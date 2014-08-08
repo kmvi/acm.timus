e6380891556996e3f5124599cd1ff7b8b9a5ede5
kmvi <kmvi@users.noreply.github.com>
2014-08-08 13:18:00 +0400

    problem 1120

---
+++ t1120/src/t1120/T1120.java
@@ -0,0 +1,30 @@
+package t1120;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class T1120 {
+
+	public static void main(String[] args) {
+		try {
+			Scanner inFile = new Scanner(System.in);
+			PrintStream outFile = new PrintStream(System.out);
+			
+			int N = inFile.nextInt();
+			
+			int p = (int)Math.floor((Math.sqrt(1.0 + 8.0 * N) - 1.0) / 2.0);
+			double a = (double)N / p - (p - 1.0) / 2.0;
+			
+			while (Math.abs(a - (int)a) > 1e-6) {
+				p--;
+				a = (double)N / p - (p - 1.0) / 2.0;
+			}
+			
+			outFile.println(String.format("%d %d\n", (int)a, p));
+			
+			inFile.close();
+			outFile.close();
+		} catch (Exception ex) {}
+	}
+
+}
