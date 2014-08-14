9ab7bc755543f789c8111a3f11f0d97d9a040ba0
kmvi <kmvi@users.noreply.github.com>
2014-08-14 14:04:16 +0400

    problem 1777

---
+++ t1777/src/t1777/T1777.java
@@ -0,0 +1,47 @@
+package t1777;
+
+import java.io.PrintStream;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class T1777 {
+
+	public static void main(String[] args) {
+		try {
+			Scanner inFile = new Scanner(System.in);
+			PrintStream outFile = new PrintStream(System.out);
+			
+			LinkedList<Long> lst = new LinkedList<>();
+			
+			for (int i = 0; i < 3; ++i)
+				lst.add(inFile.nextLong());
+			
+			long show = 1;			
+			while (true) {				
+				Collections.sort(lst);
+				show++;
+				
+				long curMin = Long.MAX_VALUE;
+				for (int i = 0; i < lst.size()-1; ++i) {
+					long x1 = lst.get(i);
+					long x2 = lst.get(i+1);
+					long m = x2 - x1;
+					if (m < curMin)
+						curMin = m;
+				}
+			
+				if (lst.contains(curMin))
+					break;
+				
+				lst.add(curMin);
+			}			
+			
+			outFile.println(show);
+			
+			inFile.close();
+			outFile.close();
+		} catch (Exception ex) {}
+	}
+
+}
