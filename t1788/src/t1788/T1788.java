c3d23bbbb8097bba626723e1fba8cf17be9e434d
kmvi <kmvi@users.noreply.github.com>
2014-08-15 09:31:45 +0400

    problem 1788

---
+++ t1788/src/t1788/T1788.java
@@ -0,0 +1,53 @@
+package t1788;
+
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class T1788 {
+
+	public static void main(String[] args) {
+		try {
+			Scanner inFile = new Scanner(System.in);
+			PrintStream outFile = new PrintStream(System.out);
+			
+			int fCount = inFile.nextInt();
+			int mCount = inFile.nextInt();
+			
+			int[] g = new int[fCount];
+			int[] b = new int[mCount];
+			
+			for (int i = 0; i < fCount; ++i)
+				g[i] = inFile.nextInt();
+			
+			for (int i = 0; i < mCount; ++i)
+				b[i] = inFile.nextInt();
+			
+			Arrays.sort(g);
+			Arrays.sort(b);
+			
+			int minSum = Integer.MAX_VALUE;
+
+			for (int k = 0; k <= (fCount <= mCount ? fCount : mCount); ++k) {
+				
+				int sum = 0;				
+				for (int i = 0; i < mCount-k; ++i)
+					sum += b[i];
+				
+				sum *= k;
+				
+				for (int i = 0; i < fCount-k; ++i)
+					sum += g[i];
+				
+				if (sum < minSum)
+					minSum = sum;
+			}
+			
+			outFile.println(minSum);
+			
+			inFile.close();
+			outFile.close();
+		} catch (Exception ex) {}
+	}
+
+}
