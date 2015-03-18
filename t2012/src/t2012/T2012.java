4b638cc5f695df7bbb47b108a689c34d567abcaa
kmvi <kmvi@users.noreply.github.com>
2015-03-18 12:49:35 +0300

    problem 2012

---
+++ t2012/src/t2012/T2012.java
@@ -0,0 +1,24 @@
+package t2012;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class T2012 {
+
+	public static void main(String[] args) {
+		try {
+			Scanner inFile = new Scanner(System.in);
+			PrintStream outFile = new PrintStream(System.out);
+			
+			int f = inFile.nextInt();			
+			int time = 4 * 60 - 45 * (12 - f);
+			
+			outFile.println(time < 0 ? "NO" : "YES");
+			
+			
+			inFile.close();
+			outFile.close();
+		} catch (Exception ex) {}
+	}
+
+}
