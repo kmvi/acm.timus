2709917117259b3d3e64226280f89fa99f2482e6
kmvi <kmvi@users.noreply.github.com>
2015-03-21 09:49:09 +0300

    problem 1021

---
+++ t1021/src/t1021/T1021.java
@@ -0,0 +1,38 @@
+package t1021;
+
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class T1021 {
+
+	public static void main(String[] args) {
+		try {
+			Scanner inFile = new Scanner(System.in);
+			PrintStream outFile = new PrintStream(System.out);
+			
+			int n1 = inFile.nextInt();
+			int[] list1 = new int[n1];
+			for (int i = 0; i < n1; ++i)
+				list1[i] = inFile.nextInt();
+			
+			int n2 = inFile.nextInt();
+			int[] list2 = new int[n2];
+			for (int i = 0; i < n2; ++i)
+				list2[n2-i-1] = inFile.nextInt();
+			
+			boolean found = false;
+			for (int i = 0; i < n1; ++i) {
+				int ind = Arrays.binarySearch(list2, 10000 - list1[i]);
+				if (ind >= 0)
+					found = true;
+			}
+			
+			outFile.println(found ? "YES" : "NO");
+			
+			inFile.close();
+			outFile.close();
+		} catch (Exception ex) {}
+	}
+
+}
